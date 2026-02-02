e9243268ba8c408072ed02a92b55ebe5ad24b8e2
sam cocquyt <samcocquyt04@gmail.com>
2026-02-02 00:15:27 -0500

    first name generator function added, test print

--- fantasy_name_generator_unit3.py
+++ fantasy_name_generator_unit3.py
@@ -18,7 +18,13 @@ first_ending    = ["wyn", "riel", "thir", "loth", "diel"]
 last_beginning = ["Moon", "Star", "Silver", "Dawn", "Night"]
 last_ending    = ["whisper", "bloom", "runner", "song", "glade"]
 
+# first name generator function
+def generate_first_name():
+    first_name = random.choice(first_beginning) + random.choice(first_middle) + random.choice(first_ending)
+    return first_name
 
+# test print
+print(generate_first_name())
 
 
 
