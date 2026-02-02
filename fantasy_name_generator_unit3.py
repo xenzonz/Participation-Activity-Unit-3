b7e5515bf66b3affdeb00ec58f4cc72656c5aca0
sam cocquyt <samcocquyt04@gmail.com>
2026-02-02 00:09:41 -0500

    docstring added, name lists added

---
+++ fantasy_name_generator_unit3.py
@@ -0,0 +1,24 @@
+""" 
+Docstring for Participation Activity Unit 3 
+i. Fantasy Name Generator
+ii. Sam Cocquyt
+iii. Generate random elf names
+iv. No starter code
+v. 2/1/2026
+"""
+
+import random
+
+# first name syllables, elf like
+first_beginning = ["Ae", "Eli", "Fa", "Lia", "Syl"]
+first_middle    = ["ri", "la", "sha", "the", "nys"]
+first_ending    = ["wyn", "riel", "thir", "loth", "diel"]
+
+# last name words, elf like
+last_beginning = ["Moon", "Star", "Silver", "Dawn", "Night"]
+last_ending    = ["whisper", "bloom", "runner", "song", "glade"]
+
+
+
+
+
